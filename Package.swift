763344d715f61f5bc1cc860c5e3db7c107302fe2
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-12-02 18:38:18 -0800

    update to lates swift packages

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .package(url: "https://github.com/JohnSundell/Files", from: "4.0.0"),
         .package(url: "https://github.com/nikolainobadi/NnGitKit.git", from: "0.6.0"),
         .package(url: "https://github.com/nikolainobadi/NnShellKit.git", from: "2.0.0"),
-        .package(url: "https://github.com/nikolainobadi/NnSwiftDataKit", from: "0.5.0"),
+        .package(url: "https://github.com/nikolainobadi/NnSwiftDataKit", branch: "main"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
         .package(url: "https://github.com/nikolainobadi/SwiftPickerKit.git", from: "0.6.0"),
     ],
