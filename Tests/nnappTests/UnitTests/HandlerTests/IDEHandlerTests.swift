e99fa99e106e5e21a885bb7fcd4d80b3270e5b89
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-12-05 15:57:36 -0800

    add more tests for IDEHandler

--- Tests/nnappTests/UnitTests/HandlerTests/IDEHandlerTests.swift
+++ Tests/nnappTests/UnitTests/HandlerTests/IDEHandlerTests.swift
@@ -24,20 +24,49 @@ struct IDEHandlerTests {
     }
 
     @Test("Opens existing project in xcode without cloning")
-    func opensExistingProjectWithoutCloning() throws {
+    func opensExistingProjectInXcodeWithoutCloning() throws {
         let projectGroup = makeProjectGroup()
-        let directoryPath = "existing/project"
         let project = makeProject(name: "Existing", group: projectGroup)
-        let (sut, shell, fileSystem) = makeSUT(directoryToLoad: .init(path: directoryPath))
+        let (sut, shell, fileSystem) = makeSUT(directoryToLoad: .init(path: try #require(project.folderPath)))
         let filePath = try #require(project.filePath)
+        let folderPath = try #require(project.folderPath)
 
         try sut.openInIDE(project, launchType: .xcode)
 
-        #expect(fileSystem.capturedPaths.count == 1)
-        #expect(shell.executedCommands.count == 1)
+        #expect(fileSystem.capturedPaths.contains(folderPath))
         #expect(shell.executedCommand(containing: "open"))
         #expect(shell.executedCommand(containing: filePath))
-        #expect(!shell.executedCommands.contains(where: { $0.contains("git clone") }))
+        #expect(!shell.executedCommand(containing: "git clone"))
+    }
+
+    @Test("Clones project when missing locally and remote exists", arguments: [LaunchType.xcode, LaunchType.vscode])
+    func clonesProjectWhenMissingLocallyAndRemoteExists(launchType: LaunchType) throws {
+        let projectGroup = makeProjectGroup(path: "/tmp/remoteGroup")
+        let projectLink = makeProjectLink(urlString: "https://github.com/example/repo")
+        let project = makeProject(name: "Clonable", remote: projectLink, group: projectGroup)
+        let (sut, shell, fileSystem) = makeSUT(directoryToLoad: nil)
+        let folderPath = try #require(project.folderPath)
+
+        try sut.openInIDE(project, launchType: launchType)
+
+        #expect(fileSystem.capturedPaths.contains(folderPath))
+        #expect(shell.executedCommand(containing: "git"))
+        #expect(shell.executedCommand(containing: "clone"))
+    }
+
+    @Test("Opens existing project in VSCode without cloning")
+    func opensExistingProjectInVSCodeWithoutCloning() throws {
+        let projectGroup = makeProjectGroup()
+        let project = makeProject(name: "ExistingVSCode", type: .package, group: projectGroup)
+        let folderPath = try #require(project.folderPath)
+        let (sut, shell, fileSystem) = makeSUT(directoryToLoad: .init(path: folderPath))
+
+        try sut.openInIDE(project, launchType: .vscode)
+
+        #expect(fileSystem.capturedPaths.contains(folderPath))
+        #expect(shell.executedCommand(containing: "code"))
+        #expect(shell.executedCommand(containing: folderPath))
+        #expect(!shell.executedCommand(containing: "git clone"))
     }
 }
 
