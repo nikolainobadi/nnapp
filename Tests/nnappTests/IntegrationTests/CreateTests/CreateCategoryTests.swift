7caa6aeb490fded65c48cc213ca51226ccb55f78
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-12-06 14:56:14 -0800

    re-enable Create command tests

--- Tests/nnappTests/IntegrationTests/CreateTests/CreateCategoryTests.swift
+++ Tests/nnappTests/IntegrationTests/CreateTests/CreateCategoryTests.swift
@@ -1,125 +1,115 @@
-////
-////  CreateCategoryTests.swift
-////  nnapp
-////
-////  Created by Nikolai Nobadi on 3/29/25.
-////
 //
-//import Testing
-//import CodeLaunchKit
-//import SwiftPickerTesting
-//@testable import nnapp
-//@preconcurrency import Files
+//  CreateCategoryTests.swift
+//  nnapp
 //
-//@MainActor
-//final class CreateCategoryTests: MainActorBaseCreateTests {
-//    private let categoryName = "categoryName"
-//    
-//    private var parentPath: String {
-//        return tempFolder.path
-//    }
-//}
+//  Created by Nikolai Nobadi on 3/29/25.
 //
-//
-//// MARK: - Unit Tests
-//extension CreateCategoryTests {
-//    @Test("Throws an error when a folder already exists in the parent directory with same name", .disabled()) // TODO: - 
-//    func throwsErrorWhenFolderAlreadyExists() throws {
-//        _ = try tempFolder.createSubfolder(named: categoryName)
-//        
-//        do {
-//            try runCategoryCommand()
-//            Issue.record("expected an error but none were thrown")
-//        } catch let codeLaunchError as CodeLaunchError {
-//            switch codeLaunchError {
-//            case .categoryPathTaken:
-//                break
-//            default:
-//                Issue.record("unexpected error")
-//            }
-//        }
-//    }
-//    
-//    @Test("Throws an error when the name is taken by an existing Category")
-//    func throwsErrorWhenCategoryNameAlreadyExists() throws {
-//        let existingCategoryFolder = try tempFolder.createSubfolder(named: categoryName)
-//        let factory = MockContextFactory()
-//        let context = try factory.makeContext()
-//        let category = makeSwiftDataCategory(name: categoryName, path: existingCategoryFolder.path)
-//
-//        try context.saveCategory(category)
-//        
-//        do {
-//            try runCategoryCommand(factory: factory)
-//            Issue.record("expected an error but none were thrown")
-//        } catch let codeLaunchError as CodeLaunchError {
-//            switch codeLaunchError {
-//            case .categoryNameTaken:
-//                break
-//            default:
-//                Issue.record("unexpected error")
-//            }
-//        }
-//    }
-//    
-//    @Test("Creates a new folder for the new category", arguments: TestInfo.testOptions)
-//    func createCategoryFolder(info: TestInfo) throws {
-//        let picker = MockSwiftPicker(inputResult: .init(type: .ordered(makeCategoryInputs(info: info))))
-//        let folderBrowser = MockDirectoryBrowser()
-//        folderBrowser.folderToReturn = tempFolder
-//        let factory = MockContextFactory(picker: picker, folderBrowser: folderBrowser)
-//
-//        try runCategoryCommand(factory: factory, info: info)
-//
-//        let updatedFolder = try Folder(path: parentPath)
-//        
-//        #expect(updatedFolder.containsSubfolder(named: categoryName))
-//    }
-//    
-//    @Test("Saves the new category", arguments: TestInfo.testOptions)
-//    func savesNewCategory(info: TestInfo) throws {
-//        let picker = MockSwiftPicker(inputResult: .init(type: .ordered(makeCategoryInputs(info: info))))
-//        let folderBrowser = MockDirectoryBrowser()
-//        folderBrowser.folderToReturn = tempFolder
-//        let factory = MockContextFactory(picker: picker, folderBrowser: folderBrowser)
-//        
-//        try runCategoryCommand(factory: factory, info: info)
-//        
-//        let categories = try factory.makeContext().loadCategories()
-//        let savedCategory = try #require(categories.first)
-//        
-//        #expect(categories.count == 1)
-//        #expect(savedCategory.groups.isEmpty)
-//        #expect(savedCategory.name == categoryName)
-//        
-//        #expect(savedCategory.path == parentPath.appendingPathComponent(categoryName))
-//    }
-//}
-//
-//
-//// MARK: - RunCommand
-//private extension CreateCategoryTests {
-//    func runCategoryCommand(factory: MockContextFactory? = nil, info: TestInfo = .init(name: .arg, otherArg: .arg)) throws {
-//        try runCommand(
-//            factory,
-//            argType: .category(
-//                name: info.name == .arg ? categoryName : nil,
-//                parentPath: info.otherArg == .arg ? parentPath : nil
-//            )
-//        )
-//    }
-//}
-//
-//
-//// MARK: - Helpers
-//private extension CreateCategoryTests {
-//    func makeCategoryInputs(info: TestInfo) -> [String] {
-//        var inputs = [String]()
-//        
-//        if info.name == .input {
-//            inputs.append(categoryName)
-//        }
-//        
-//        return inputs
-//    }
-//}
+
+import Files
+import Testing
+import CodeLaunchKit
+import SwiftPickerTesting
+@testable import nnapp
+
+private let categoryName = "categoryName"
+
+@MainActor
+final class CreateCategoryTests: MainActorBaseCreateTests {
+    private var parentPath: String {
+        return tempFolder.path
+    }
+}
+
+
+// MARK: - Unit Tests
+extension CreateCategoryTests {
+    @Test("Throws an error when a folder already exists in the parent directory with same name")
+    func throwsErrorWhenFolderAlreadyExists() throws {
+        _ = try tempFolder.createSubfolder(named: categoryName)
+        let parentPath = parentPath
+        let selectedDirectory = FilesDirectoryAdapter(folder: tempFolder)
+        let folderBrowser = MockDirectoryBrowser(selectedDirectory: selectedDirectory)
+        let factory = MockContextFactory(folderBrowser: folderBrowser)
+
+        #expect(throws: CodeLaunchError.categoryPathTaken) {
+            try runCategoryCommand(factory: factory, parentPath: parentPath)
+        }
+    }
+
+    @Test("Throws an error when the name is taken by an existing Category")
+    func throwsErrorWhenCategoryNameAlreadyExists() throws {
+        let parentPath = parentPath
+        let existingCategoryFolder = try tempFolder.createSubfolder(named: categoryName)
+        let selectedDirectory = FilesDirectoryAdapter(folder: tempFolder)
+        let folderBrowser = MockDirectoryBrowser(selectedDirectory: selectedDirectory)
+        let factory = MockContextFactory(folderBrowser: folderBrowser)
+        let context = try factory.makeContext()
+        let category = makeSwiftDataCategory(name: categoryName, path: existingCategoryFolder.path)
+
+        try context.saveCategory(category)
+
+        #expect(throws: CodeLaunchError.categoryNameTaken) {
+            try runCategoryCommand(factory: factory, parentPath: parentPath)
+        }
+    }
+
+    @Test("Creates a new folder for the new category", arguments: MainActorBaseCreateTests.TestInfo.testOptions)
+    func createCategoryFolder(info: MainActorBaseCreateTests.TestInfo) throws {
+        let picker = MockSwiftPicker(inputResult: .init(type: .ordered(makeCategoryInputs(info: info))))
+        let selectedDirectory = FilesDirectoryAdapter(folder: tempFolder)
+        let folderBrowser = MockDirectoryBrowser(selectedDirectory: selectedDirectory)
+        let factory = MockContextFactory(picker: picker, folderBrowser: folderBrowser)
+
+        try runCategoryCommand(factory: factory, info: info, parentPath: parentPath)
+
+        let updatedFolder = try Folder(path: parentPath)
+
+        #expect(updatedFolder.containsSubfolder(named: categoryName))
+    }
+
+    @Test("Saves the new category", arguments: MainActorBaseCreateTests.TestInfo.testOptions)
+    func savesNewCategory(info: MainActorBaseCreateTests.TestInfo) throws {
+        let picker = MockSwiftPicker(inputResult: .init(type: .ordered(makeCategoryInputs(info: info))))
+        let selectedDirectory = FilesDirectoryAdapter(folder: tempFolder)
+        let folderBrowser = MockDirectoryBrowser(selectedDirectory: selectedDirectory)
+        let factory = MockContextFactory(picker: picker, folderBrowser: folderBrowser)
+
+        try runCategoryCommand(factory: factory, info: info, parentPath: parentPath)
+
+        let categories = try factory.makeContext().loadCategories()
+        let savedCategory = try #require(categories.first)
+
+        #expect(categories.count == 1)
+        #expect(savedCategory.groups.isEmpty)
+        #expect(savedCategory.name == categoryName)
+
+        #expect(savedCategory.path == parentPath.appendingPathComponent(categoryName))
+    }
+}
+
+
+// MARK: - Helpers
+private extension CreateCategoryTests {
+    func makeCategoryInputs(info: MainActorBaseCreateTests.TestInfo) -> [String] {
+        var inputs = [String]()
+
+        if info.name == .input {
+            inputs.append(categoryName)
+        }
+
+        return inputs
+    }
+}
+
+
+// MARK: - Run
+@MainActor
+private func runCategoryCommand(factory: MockContextFactory? = nil, info: MainActorBaseCreateTests.TestInfo = .init(name: .arg, otherArg: .arg), parentPath: String? = nil) throws {
+    try MainActorBaseCreateTests.runCreateCommand(
+        factory,
+        argType: .category(
+            name: info.name == .arg ? categoryName : nil,
+            parentPath: info.otherArg == .arg ? parentPath : nil
+        )
+    )
+}
--- Tests/nnappTests/IntegrationTests/CreateTests/CreateGroupTests.swift
+++ Tests/nnappTests/IntegrationTests/CreateTests/CreateGroupTests.swift
@@ -1,114 +1,101 @@
-////
-////  CreateGroupTests.swift
-////  nnapp
-////
-////  Created by Nikolai Nobadi on 3/28/25.
-////
 //
-//import Testing
-//import CodeLaunchKit
-//@testable import nnapp
-//@preconcurrency import Files
+//  CreateGroupTests.swift
+//  nnapp
 //
-//@MainActor
-//final class CreateGroupTests: MainActorBaseCreateTests {
-//    private let existingGroupName = "existingGroupName"
-//    private let existingCategoryName = "existingCategoryName"
-//    
-//    init() throws {
-//        let testGroupFolder = TestFolder(name: existingGroupName, subFolders: [])
-//        let testCategoryFolder = TestFolder(name: existingCategoryName, subFolders: [testGroupFolder])
-//        
-//        try super.init(testFolder: .init(name: "createGroupCategoryList", subFolders: [testCategoryFolder]))
-//    }
-//}
+//  Created by Nikolai Nobadi on 3/28/25.
 //
-//
-//// MARK: - Unit Tests
-//extension CreateGroupTests {
-//    @Test("Throws error when no Category is selected")
-//    func throwsErrorWhenNoCategorySelected() {
-//        do {
-//            try runGroupCommand()
-//            Issue.record("expected an error but none were thrown")
-//        } catch { }
-//    }
-//    
-//    @Test("Throws error when name is taken by existing Group")
-//    func throwsErrorWhenGroupNameIsTaken() throws {
-//        let factory = try makeFactory(includeGroup: true)
-//        
-//        do {
-//            try runGroupCommand(factory: factory, name: existingGroupName, category: existingCategoryName)
-//            Issue.record("expected an error but none were thrown")
-//        } catch let codeLaunchError as CodeLaunchError {
-//            switch codeLaunchError {
-//            case .groupNameTaken:
-//                break
-//            default:
-//                Issue.record("unexpected error")
-//            }
-//        }
-//    }
-//    
-//    @Test("Throws error when name is taken by existing folder in Category folder")
-//    func throwsErrorWhenGroupFolderNameIsTaken() throws {
-//        let factory = try makeFactory(includeGroup: false)
-//        
-//        do {
-//            try runGroupCommand(factory: factory, name: existingGroupName, category: existingCategoryName)
-//            Issue.record("expected an error but none were thrown")
-//        } catch let codeLaunchError as CodeLaunchError {
-//            switch codeLaunchError {
-//            case .groupFolderAlreadyExists:
-//                break
-//            default:
-//                Issue.record("unexpected error")
-//            }
-//        }
-//    }
-//    
-//    @Test("Creates a new Group folder in an existing Category folder")
-//    func createsNewGroupFolderInExistingCategoryFolder() throws {
-//        let groupName = "newGroupName"
-//        let factory = try makeFactory()
-//
-//        try runGroupCommand(factory: factory, name: groupName, category: existingCategoryName)
-//
-//        let context = try factory.makeContext()
-//        let newGroup = try #require(context.loadGroups().first)
-//        let newGroupPath = try #require(newGroup.path)
-//        let updatedCategoryFolder = try tempFolder.subfolder(named: existingCategoryName)
-//        let newGroupFolder = try updatedCategoryFolder.subfolder(named: groupName)
-//
-//        #expect(newGroup.name == groupName)
-//        #expect(newGroupPath == newGroupFolder.path)
-//    }
-//}
-//
-//
-//// MARK: - Factory
-//private extension CreateGroupTests {
-//    func makeFactory(includeGroup: Bool = false) throws -> MockContextFactory {
-//        let existingCategoryFolder = try tempFolder.subfolder(named: existingCategoryName)
-//        let factory = MockContextFactory()
-//        let context = try factory.makeContext()
-//        let category = makeSwiftDataCategory(name: existingCategoryName, path: existingCategoryFolder.path)
-//        try context.saveCategory(category)
-//
-//        if includeGroup {
-//            try context.saveGroup(makeSwiftDataGroup(name: existingGroupName), in: category)
-//        }
-//
-//        return factory
-//    }
-//}
-//
-//
-//
-//// MARK: - RunCommand
-//private extension CreateGroupTests {
-//    func runGroupCommand(factory: MockContextFactory? = nil, name: String? = nil, category: String? = nil) throws {
-//        try runCommand(factory, argType: .group(name: name, category: category))
-//    }
-//}
+
+import Files
+import Testing
+import CodeLaunchKit
+import SwiftPickerTesting
+@testable import nnapp
+
+private let existingGroupName = "existingGroupName"
+private let existingCategoryName = "existingCategoryName"
+
+@MainActor
+final class CreateGroupTests: MainActorBaseCreateTests {
+    init() throws {
+        let testGroupFolder = TestFolder(name: existingGroupName, subFolders: [])
+        let testCategoryFolder = TestFolder(name: existingCategoryName, subFolders: [testGroupFolder])
+
+        try super.init(testFolder: .init(name: "createGroupCategoryList", subFolders: [testCategoryFolder]))
+    }
+}
+
+
+// MARK: - Unit Tests
+extension CreateGroupTests {
+    @Test("Throws error when no Category is selected")
+    func throwsErrorWhenNoCategorySelected() throws {
+        let folderBrowser = MockDirectoryBrowser()
+        let factory = MockContextFactory(folderBrowser: folderBrowser)
+
+        #expect(throws: (any Error).self) {
+            try runGroupCommand(factory: factory)
+        }
+    }
+
+    @Test("Throws error when name is taken by existing Group")
+    func throwsErrorWhenGroupNameIsTaken() throws {
+        let factory = try makeFactory(includeGroup: true)
+
+        #expect(throws: CodeLaunchError.groupNameTaken) {
+            try runGroupCommand(factory: factory, name: existingGroupName, category: existingCategoryName)
+        }
+    }
+
+    @Test("Throws error when name is taken by existing folder in Category folder")
+    func throwsErrorWhenGroupFolderNameIsTaken() throws {
+        let factory = try makeFactory(includeGroup: false)
+
+        #expect(throws: CodeLaunchError.groupFolderAlreadyExists) {
+            try runGroupCommand(factory: factory, name: existingGroupName, category: existingCategoryName)
+        }
+    }
+
+    @Test("Creates a new Group folder in an existing Category folder")
+    func createsNewGroupFolderInExistingCategoryFolder() throws {
+        let groupName = "newGroupName"
+        let factory = try makeFactory()
+
+        try runGroupCommand(factory: factory, name: groupName, category: existingCategoryName)
+
+        let context = try factory.makeContext()
+        let newGroup = try #require(context.loadGroups().first)
+        let newGroupPath = try #require(newGroup.path)
+        let updatedCategoryFolder = try tempFolder.subfolder(named: existingCategoryName)
+        let newGroupFolder = try updatedCategoryFolder.subfolder(named: groupName)
+
+        #expect(newGroup.name == groupName)
+        #expect(newGroupPath == newGroupFolder.path)
+    }
+}
+
+
+// MARK: - Factory
+private extension CreateGroupTests {
+    func makeFactory(includeGroup: Bool = false) throws -> MockContextFactory {
+        let existingCategoryFolder = try tempFolder.subfolder(named: existingCategoryName)
+        let folderBrowser = MockDirectoryBrowser()
+        folderBrowser.selectedDirectory = FilesDirectoryAdapter(folder: existingCategoryFolder)
+        let factory = MockContextFactory(folderBrowser: folderBrowser)
+        let context = try factory.makeContext()
+        let category = makeSwiftDataCategory(name: existingCategoryName, path: existingCategoryFolder.path)
+        try context.saveCategory(category)
+
+        if includeGroup {
+            try context.saveGroup(makeSwiftDataGroup(name: existingGroupName), in: category)
+        }
+
+        return factory
+    }
+}
+
+
+// MARK: - Run
+@MainActor
+private func runGroupCommand(factory: MockContextFactory? = nil, name: String? = nil, category: String? = nil) throws {
+    try MainActorBaseCreateTests.runCreateCommand(factory, argType: .group(name: name, category: category))
+}
